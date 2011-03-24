6bc77bf65055c88a1a5c91f9f97b9ecf98df5624
Ilan Schnell <ischnell@enthought.com>
2011-03-23 21:17:53 -0500

    update work on example, update changelog

--- examples/update/main.py
+++ examples/update/main.py
@@ -1,25 +1,34 @@
+import os
 import zipfile
 from compiler import compileFile
 from os.path import join
 
 
-
 def build_egg(name):
     py_module = 'update_%s.py' % name
 
+    code = open('update_NAME.py').read()
+    code = code.replace('@NAME@', name)
+    open(py_module, 'w').write(code)
     compileFile(py_module)
+
     z = zipfile.ZipFile(join('update_%s-1.0.egg' % name),
                         'w', zipfile.ZIP_DEFLATED)
-    z.write('appinst.dat', 'EGG-INFO/inst/appinst.dat')
-    z.write('update.ico', 'EGG-INFO/inst/update.ico')
     for ext in ('', 'c'):
         z.write(py_module + ext)
-    z.writestr('EGG-INFO/entry_points.txt', '''\
-[console_scripts]
-update-%s = update_$s:main
-    ''' % (name, name))
 
+    data = open('appinst.dat').read()
+    data = data.replace('@NAME@', name)
+    z.writestr('EGG-INFO/inst/appinst.dat', data)
+
+    z.writestr('EGG-INFO/entry_points.txt',
+               '[console_scripts]\n'
+               'update-%s = update_%s:main\n' % (name, name))
+    z.write('update.ico', 'EGG-INFO/inst/update.ico')
     z.close()
+
+    for ext in ('', 'c'):
+        os.unlink(py_module + ext)
 
 
 if __name__ == '__main__':
