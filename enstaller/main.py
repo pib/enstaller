42824a7460fb8e234971ec7bdea9c66995feea34
Ilan Schnell <ischnell@enthought.com>
2011-06-26 01:36:30 -0500

    add module for currently unused functions

--- enstaller/main.py
+++ enstaller/main.py
@@ -10,7 +10,6 @@ import sys
 import string
 import subprocess
 import textwrap
-from collections import defaultdict
 from os.path import basename, isdir, isfile, join
 from optparse import OptionParser
 
@@ -70,55 +69,6 @@ def egginst_subprocess(pkg_path, remove):
     if verbose:
         print 'CALL: %r' % args
     subprocess.call(args)
-
-
-def get_status():
-    # the result is a dict mapping cname to ...
-    res = {}
-    for cname in egginst.get_installed_cnames(sys.prefix):
-        d = defaultdict(str)
-        info = get_installed_info(sys.prefix, cname)
-        if info is None:
-            continue
-        d.update(info)
-        res[cname] = d
-
-    conf = config.get()
-    c = Chain(conf['IndexedRepos'])
-
-    for cname in c.groups.iterkeys():
-        dist = c.get_dist(Req(cname))
-        if dist is None:
-            continue
-        repo, fn = dist_naming.split_dist(dist)
-        n, v, b = dist_naming.split_eggname(fn)
-        if cname not in res:
-            d = defaultdict(str)
-            d['name'] = n
-            res[cname] = d
-        res[cname]['a-egg'] = fn
-        res[cname]['a-ver'] = '%s-%d' % (v, b)
-
-    def vb_egg(fn):
-        try:
-            n, v, b = dist_naming.split_eggname(fn)
-            return comparable_version(v), b
-        except:
-            return None
-
-    for d in res.itervalues():
-        if d['egg_name']:                    # installed
-            if d['a-egg']:
-                if vb_egg(d['egg_name']) >= vb_egg(d['a-egg']):
-                    d['status'] = 'up-to-date'
-                else:
-                    d['status'] = 'updateable'
-            else:
-                d['status'] = 'installed'
-        else:                                # not installed
-            if d['a-egg']:
-                d['status'] = 'installable'
-    return res
 
 
 def egginst_remove(pkg):
@@ -660,5 +610,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    #for v in get_status().itervalues():
-    #    print '%(name)-20s %(version)16s %(a-ver)16s %(status)12s' % v
---
+++ enstaller/unused.py
@@ -0,0 +1,62 @@
+import sys
+from collections import defaultdict
+
+import egginst
+
+from indexed_repo import Chain, Req, dist_naming
+from utils import get_installed_info, comparable_version
+import config
+
+
+def get_status():
+    # the result is a dict mapping cname to ...
+    res = {}
+    for cname in egginst.get_installed_cnames(sys.prefix):
+        d = defaultdict(str)
+        info = get_installed_info(sys.prefix, cname)
+        if info is None:
+            continue
+        d.update(info)
+        res[cname] = d
+
+    conf = config.get()
+    c = Chain(conf['IndexedRepos'])
+
+    for cname in c.groups.iterkeys():
+        dist = c.get_dist(Req(cname))
+        if dist is None:
+            continue
+        repo, fn = dist_naming.split_dist(dist)
+        n, v, b = dist_naming.split_eggname(fn)
+        if cname not in res:
+            d = defaultdict(str)
+            d['name'] = n
+            res[cname] = d
+        res[cname]['a-egg'] = fn
+        res[cname]['a-ver'] = '%s-%d' % (v, b)
+
+    def vb_egg(fn):
+        try:
+            n, v, b = dist_naming.split_eggname(fn)
+            return comparable_version(v), b
+        except:
+            return None
+
+    for d in res.itervalues():
+        if d['egg_name']:                    # installed
+            if d['a-egg']:
+                if vb_egg(d['egg_name']) >= vb_egg(d['a-egg']):
+                    d['status'] = 'up-to-date'
+                else:
+                    d['status'] = 'updateable'
+            else:
+                d['status'] = 'installed'
+        else:                                # not installed
+            if d['a-egg']:
+                d['status'] = 'installable'
+    return res
+
+
+if __name__ == '__main__':
+    for v in get_status().itervalues():
+        print '%(name)-20s %(version)16s %(a-ver)16s %(status)12s' % v
