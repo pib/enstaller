060b122ca337b1285d0c206bf36222375228bcd9
Ilan Schnell <ischnell@enthought.com>
2011-08-10 22:40:37 -0500

    add get_status method

--- enstaller/resource.py
+++ enstaller/resource.py
@@ -1,10 +1,12 @@
+import sys
 import json
+from collections import defaultdict
 from os.path import expanduser, join
 
 from plat import custom_plat
-
+from utils import get_installed_info
 from indexed_repo.chain import Chain, Req
-from indexed_repo.dist_naming import split_eggname
+from indexed_repo.dist_naming import split_dist, split_eggname
 from indexed_repo.requirement import add_Reqs_to_spec
 
 
@@ -61,9 +63,59 @@ class Resources(object):
                 self.chain.groups[cname].append(dist)
 
 
+    def get_status(self):
+        import egginst
+        # the result is a dict mapping cname to ...
+        res = {}
+        for cname in egginst.get_installed_cnames(sys.prefix):
+            d = defaultdict(str)
+            info = get_installed_info(sys.prefix, cname)
+            if info is None:
+                continue
+            d.update(info)
+            res[cname] = d
+
+            c = self.chain
+            for cname in c.groups.iterkeys():
+                dist = c.get_dist(Req(cname))
+                if dist is None:
+                    continue
+                repo, fn = split_dist(dist)
+                n, v, b = split_eggname(fn)
+                if cname not in res:
+                    d = defaultdict(str)
+                    d['name'] = n
+                    res[cname] = d
+                res[cname]['a-egg'] = fn
+                res[cname]['a-ver'] = '%s-%d' % (v, b)
+
+        def vb_egg(fn):
+            try:
+                n, v, b = dist_naming.split_eggname(fn)
+                return comparable_version(v), b
+            except:
+                return None
+
+        for d in res.itervalues():
+            if d['egg_name']:                    # installed
+                if d['a-egg']:
+                    if vb_egg(d['egg_name']) >= vb_egg(d['a-egg']):
+                        d['status'] = 'up-to-date'
+                    else:
+                        d['status'] = 'updateable'
+                else:
+                    d['status'] = 'installed'
+            else:                                # not installed
+                if d['a-egg']:
+                    d['status'] = 'installable'
+        return res
+
+
 if __name__ == '__main__':
     r = Resources(['file://' + expanduser('~/buildware/scripts')])
 
     req = Req('epd')
     print r.chain.get_dist(req)
     r.chain.print_repos()
+    for v in r.get_status().itervalues():
+        print '%(name)-20s %(version)16s %(a-ver)16s %(status)12s' % v
