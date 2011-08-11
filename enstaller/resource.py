d8cd168d63467940abd4438d631cbeed7c3f6dc7
Ilan Schnell <ischnell@enthought.com>
2011-08-10 22:11:29 -0500

    minor improvements and cleanup

--- enstaller/resource.py
+++ enstaller/resource.py
@@ -1,7 +1,7 @@
 import json
-from os.path import dirname, expanduser, join
+from os.path import expanduser, join
 
-from custom_tools import platform
+from plat import custom_plat
 
 from indexed_repo.chain import Chain, Req
 from indexed_repo.dist_naming import split_eggname
@@ -28,33 +28,35 @@ class Resources(object):
 
         if url.startswith('file://'):
             path = url[7:]
-            fi = open(join(path, 'index-%s.json' % platform))
+            fi = open(join(path, 'index-%s.json' % custom_plat))
             index = json.load(fi)
             fi.close()
         else:
             raise Exception
 
         if 'eggs' in index:
-            self.add_eggs(url, index)
+            self._add_egg_repos(url, index)
 
         self.index.append(index)
 
 
-    def add_eggs(self, url, index):
-        repos = [url + path + '/' for path in index['egg_repos']]
+    def _add_egg_repos(self, url, index):
+        if 'egg_repos' in index:
+            repos = [url + path + '/' for path in index['egg_repos']]
+        else:
+            repos = [url]
         self.chain.repos.extend(repos)
 
         for cname, project in index['eggs'].iteritems():
-            files = project['files']
-            for fn, data in files.iteritems():
-                name, version, build = split_eggname(fn)
+            for distname, data in project['files'].iteritems():
+                name, version, build = split_eggname(distname)
                 spec = dict(metadata_version='1.1',
                             name=name, version=version, build=build,
                             python=data.get('python', '2.7'),
                             packages=data.get('depends', []))
                 add_Reqs_to_spec(spec)
-                repo_i = data.get('repo', 0)
-                dist = repos[repo_i] + fn
+                assert spec['cname'] == cname, distname
+                dist = repos[data.get('repo', 0)] + distname
                 self.chain.index[dist] = spec
                 self.chain.groups[cname].append(dist)
 
@@ -62,5 +64,6 @@ class Resources(object):
 if __name__ == '__main__':
     r = Resources(['file://' + expanduser('~/buildware/scripts')])
 
-    req = Req('epd 7.1-2')
+    req = Req('epd')
     print r.chain.get_dist(req)
+    r.chain.print_repos()
