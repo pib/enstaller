8e225107587e210390bd33fbcd19d9c7a644c2f4
Ilan Schnell <ischnell@enthought.com>
2011-12-02 12:23:59 -0600

    fix in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages = [
         'egginst',
         'enstaller',
-        'enstaller.repo',
+        'enstaller/store',
         'enstaller/indexed_repo',
         'enstaller/proxy',
     ],
