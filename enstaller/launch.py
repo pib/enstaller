d6ed4bbfa4b81b0b7d2a22686d2992e3d2687a76
Ilan Schnell <ischnell@enthought.com>
2011-12-06 11:43:52 -0600

    add create_joined_store()

--- enstaller/launch.py
+++ enstaller/launch.py
@@ -2,10 +2,13 @@ import sys
 import json
 import subprocess
 from glob import glob
-from os.path import isdir, isfile, join
+from os.path import abspath, isdir, isfile, join
 
 import egginst
 from egginst.utils import pprint_fn_action, console_progress
+
+from store.indexed import LocalIndexedStore, RemoteHTTPIndexedStore
+from store.joined import JoinedStore
 
 from egg_meta import split_eggname
 from plat import custom_plat
@@ -13,11 +16,27 @@ import resolve
 import fetch
 
 
+def create_joined_store(urls):
+    stores = []
+    for url in urls:
+        if url.startswith('file://'):
+            stores.append(LocalIndexedStore(url[7:]))
+        elif url.startswith(('http://', 'https://')):
+            stores.append(RemoteHTTPIndexedStore(url))
+        elif isdir(url):
+            stores.append(LocalIndexedStore(url))
+        else:
+            raise Exception("cannot create store from URL: %r" % url)
+    return JoinedStore(stores)
+
+
 class Launch(object):
 
-    def __init__(self, remote, prefix=sys.prefix, plat=custom_plat,
+    def __init__(self, urls, userpass=None,
+                 prefix=sys.prefix, plat=custom_plat,
                  verbose=False):
-        self.remote = remote
+        self.remote = create_joined_store(urls)
+        self.userpass = auserpass
         self.prefix = prefix
         self.plat = custom_plat
         self.verbose = verbose
@@ -33,9 +52,21 @@ class Launch(object):
             info = json.load(open(p))
             yield info['key'], info
 
+    def get_all_apps(self):
+        self._connect()
+        d = dict(rem.query(app=True))
+        d.update(get_installed_apps(self))
+
+    def get_icon_path(self, egg):
+        info = self.info_installed_app(egg)
+        if 'app_icon' in info:
+            path = abspath(join(self.egginfo_dir_egg(egg), info['app_icon']))
+            if isfile(path):
+                return path
+        return None
+
     def launch_app(self, egg):
-        info = json.load(open(
-                join(self.egginfo_dir_egg(egg), 'app_meta.json')))
+        info = self.info_installed_app(egg)
         if 'app_cmd' in info:
             cmd = info['app_cmd']
         elif 'app_entry' in info:
@@ -50,7 +81,20 @@ class Launch(object):
     def install_app(self, egg, force=False):
         self.install_recur(egg, True, force)
 
+    def info_installed_app(self, egg):
+        meta_path = join(self.egginfo_dir_egg(egg), 'app_meta.json')
+        return json.load(open(meta_path))
+
+    # --------------------------------------------------------------
+
+    def _connect(self):
+        if hasattr(self, '_connected'):
+            return
+        self.remote.connect(self.userpass)
+        self._connected = True
+
     def install_recur(self, egg, hook, force=False):
+        self._connect()
         info = self.remote.get_metadata(egg)
         # todo handle python version
         resolver = resolve.Resolve(self.remote, self.verbose)
@@ -90,6 +134,7 @@ class Launch(object):
         return join(self.pkgs_dir, '%s-%s-%d' % (n.lower(), v, b))
 
     def fetch_egg(self, egg, force=False):
+        self._connect()
         f = fetch.FetchAPI(self.remote, self.local_dir)
         f.action_callback = self.action_callback
         f.progress_callback = self.progress_callback
@@ -101,16 +146,18 @@ if __name__ == '__main__':
     from store.indexed import LocalIndexedStore
     from store.joined import JoinedStore
 
-    rem = JoinedStore([LocalIndexedStore('/Users/ischnell/repo'),
-                       LocalIndexedStore('/Users/ischnell/repo2')])
-    #rem = LocalIndexedStore('/home/ischnell/eggs/')
-    rem.connect()
-    prefix = '/Users/ischnell/jpm/Python-2.7'#.2-1'
-    x = Launch(rem, prefix=prefix)#, verbose=1)
+    #rem = JoinedStore([LocalIndexedStore('/Users/ischnell/repo'),
+    #                   LocalIndexedStore('/Users/ischnell/repo2')])
+    #prefix = '/Users/ischnell/jpm/Python-2.7'#.2-1'
+    #x = Launch(rem, prefix=prefix)#, verbose=1)
+    x = Launch(['/home/ischnell/eggs/'],
+               verbose=1)
     fn = 'nose-1.1.2-1.egg'
     #x.install('enstaller-4.5.0-1.egg')
     #x.remove('enstaller-4.5.0-1.egg')
-    x.install_app(fn)#, force=1)
+#    x.install_app(fn, force=1)
 #    for d in x.get_installed_apps():
 #        print d
     x.launch_app(fn)
+    #print dict(rem.query(app=True))
+    print x.get_icon_path(fn)
