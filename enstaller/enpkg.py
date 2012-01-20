e005d4a33c0d05f5dae4fe8b7cbf856de04b2ef4
Ilan Schnell <ischnell@enthought.com>
2012-01-19 19:02:33 -0600

    add reconnect() to Enpkg class

--- enstaller/enpkg.py
+++ enstaller/enpkg.py
@@ -59,8 +59,14 @@ class Enpkg(object):
 
     # ============= methods which relate to remove store =================
 
-    def _connect(self):
+    def reconnect(self):
         self.remote.connect(self.userpass)
+
+    def _connect(self):
+        if getattr(self, '_connected', None):
+            return
+        self.remote.connect(self.userpass)
+        self._connected = True
 
     def query_remote(self, **kwargs):
         self._connect()
