766e1e7315ca7c1fad5e773769a7d1ab13bebce0
Ilan Schnell <ischnell@enthought.com>
2012-02-27 12:11:04 -0600

    simplified code to determine which eggs to fetch

--- enstaller/enpkg.py
+++ enstaller/enpkg.py
@@ -243,11 +243,9 @@ class Enpkg(object):
                 eggs = rm(eggs)
 
         res = []
-        for egg in eggs[:-1]:
-            if not isfile(join(self.local_dir, egg)):
-                res.append(('fetch_%d' % bool(forceall or force), egg))
-        if forceall or force:
-            res.append(('fetch_1', eggs[-1]))
+        for egg in eggs:
+            res.append(('fetch_%d' % bool(forceall or force), egg))
+
         if not self.hook:
             # remove packages with the same name (from first egg collection
             # only, in reverse install order)
